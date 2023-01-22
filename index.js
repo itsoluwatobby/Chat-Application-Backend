9999d613a6190610e1fef8d23a70ca85b43c2ddb
itsoluwatobby <oluwasamuel050@gmail.com>
2023-01-22 16:51:30 +0100

    modified

--- index.js
+++ index.js
@@ -42,34 +42,27 @@ const io = new Server(http, {
 
     socket.on('start-conversation', conversationId => {
 
-      socket.join(conversationId)
-
-      socket.on('leave_previous', prevConvo => {
-        socket.leave(prevConvo)
-        console.log('user left previous chat')
-      })
+      conversationId && socket.join(conversationId)
 
       socket.on('typing', infoData => {
-        socket.broadcast.to(infoData?.conversationId).emit('typing-event', infoData)
+        conversationId && socket.broadcast.to(infoData?.conversationId).emit('typing-event', infoData)
       })
 
       socket.on('no-typing', infoData => {
-        socket.broadcast.to(infoData?.conversationId).emit('typing-stop', infoData)
+        conversationId && socket.broadcast.to(infoData?.conversationId).emit('typing-stop', infoData)
       })
 
       socket.on('chat_opened', bool => {
-        console.log('opened:',bool.isChatOpened)
-        io.to(bool.userId).emit('isOpened', bool.isChatOpened)
+        conversationId && io.to(bool.userId).emit('isOpened', bool.isChatOpened)
       })
 
       socket.on('chat_closed', bool => {
-        console.log('closed:',bool.isChatOpen)
-        io.to(bool.userId).emit('isClosed', bool.isChatOpen)
+        conversationId && io.to(bool.userId).emit('isClosed', bool.isChatOpen)
       })
     
       socket.on('create_message', message => {
         //io.to(message?.conversationId).emit('new_message', message)
-        socket.broadcast.to(conversationId).emit('new_message', message)
+        conversationId && socket.broadcast.to(message?.conversationId).emit('new_message', message)
       })
     
       socket.on('disconnect', () => {
@@ -78,15 +71,15 @@ const io = new Server(http, {
     })
 
     socket.on('start_room_conversation', roomName => {
-      socket.leave(roomName)
-      socket.join(roomName)
 
-      socket.emit('new_room', {groupName: roomName, message: `You created group ${roomName}`})
-      socket.broadcast.to(roomName).emit('welcome_users', {username: 'Admin', message: `welcome to room ${roomName}`})
+      roomName && socket.join(roomName)
+
+      roomName && socket.emit('new_room', {groupName: roomName, message: `You created group ${roomName}`})
+      roomName && socket.broadcast.to(roomName).emit('welcome_users', {username: 'Admin', message: `welcome to room ${roomName}`})
 
       socket.on('new_user', data => {
-        io.to(data?.userId).emit('welcome_user', `welcome to room ${roomName}`)
-        socket.broadcast.to(roomName).except([data?.userId, data?.adminId]).emit('other_users', 'New member ${data.name} joined')
+        roomName && io.to(data?.userId).emit('welcome_user', `welcome to room ${roomName}`)
+        roomName && socket.broadcast.to(roomName).except([data?.userId, data?.adminId]).emit('other_users', 'New member ${data.name} joined')
       })
 
       socket.on('disconnect', () => {
@@ -94,23 +87,20 @@ const io = new Server(http, {
       })
     })
 
-    socket.on('conversation', datas => {
-      socket.leave(datas?._id)
-      socket.join(datas?._id)
-console.log('user joined:', datas?._id)
+    socket.on('conversation', chatApp => {
+
+      chatApp && socket.join(chatApp)
+      
       socket.on('create_conversation', convoData => {
-        console.log(convoData)
-        //io.to(convoData?._id).emit('new_conversation', convoData)
-        socket.broadcast.to(datas?._id).emit('new_conversation', convoData)
+        chatApp && socket.broadcast.to(convoData?.room).emit('new_conversation', convoData?.convo)
       })
 
       socket.on('delete_conversation', convoData => {
-        socket.broadcast.to(convoData?.otherId).emit('newDel_conversation', convoData)
+        chatApp && socket.broadcast.to(convoData?.room).emit('newDel_conversation', convoData?.convo)
       })
 
       socket.on('disconnect', () => {
-        socket.leave(datas?._id)
-        console.log('user left left left:', datas?._id)
+        socket.leave(chatApp)
       })
     })
 
--- models/Conversation.js
+++ models/Conversation.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const conversationSchema = new mongoose.Schema({
     members: {type: Array, default: []},
     adminId: {type: mongoose.Schema.Types.ObjectId, ref: 'users'},
-    createdTime: {type: String, default: ''}
+    createdTime: {type: String, default: ''},
   },
   {minimize: false},
   {timestamps: true}
